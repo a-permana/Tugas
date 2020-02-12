ff375f8d20580ce8a8b9ea6c8df354782f0078f4
Alek Permana <1alekpermana@gmail.com>
2020-02-12 18:47:19 +0700

    menambahkan file tugas satu

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const PORT = 3001;
+const ADDRESS = "localhost";
+const Mongoose = require("mongoose");
+const app = express();
+
+app.use(express.json());
+require("./tugassatu/books.js")(app);
+
+Mongoose.connect("mongodb://localhost/express_mongoose");
+
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port: http://${ADDRESS}:${PORT}`);
+});
