7b378904c8d8dbfe85aba0e2b887cb15a976941d
Alek Permana <1alekpermana@gmail.com>
2020-02-14 16:12:34 +0700

    edit server

--- server.js
+++ server.js
@@ -8,34 +8,37 @@ app.use(morgan('common'))
 
 require('./router/router.js')(app);
 
-const db = require('./config/db.js');
-const Role = db.role;
+// const db = require('./config/db.js');
+// const Role = db.role;
 
 // force: true will drop the table if it already exists (comment this part after
 // first run, to disable migration)
-db.sequelize.sync({ force: true }).then(() => {
-console.log('Drop and Resync with { force: true }');
-initial();
-});
+
+// db.sequelize.sync({ force: true }).then(() => {
+// console.log('Drop and Resync with { force: true }');
+// initial();
+// });
+
 // require('./app/route/project.route.js')(app);
 // Create a Server
+
 var server = app.listen(8080, "127.0.0.1", function() {
 var host = server.address().address;
 var port = server.address().port;
 console.log("App listening at http://%s:%s", host, port);
 });
 
-function initial() {
-Role.create({
-    id: 1,
-    name: "USER"
-    });
-Role.create({
-    id: 2,
-    name: "ADMIN"
-    });
-Role.create({
-    id: 3,
-    name: "PM"
-    });
-}no
+// function initial() {
+// Role.create({
+//     id: 1,
+//     name: "USER"
+//     });
+// Role.create({
+//     id: 2,
+//     name: "ADMIN"
+//     });
+// Role.create({
+//     id: 3,
+//     name: "PM"
+//     });
+// }
